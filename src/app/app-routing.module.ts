7ed29259c320c375541aeeae80350b8d675c9b51
Yohan491 <63322945+Yohan491@users.noreply.github.com>
2021-09-30 23:09:04 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserioComponent } from './usuario/usuario.component';
+import { AuthGuard } from './_helpers/auth.guard';
+
+
+
+const routes: Routes = [
+  { path: 'usuario', component: UserioComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
